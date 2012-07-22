07d1aca6452c49dc0589d1c8582bf57f6cbb27ca
Tom Close <tclose@oist.jp>
2012-07-22 18:53:51 -0400

    Added file and folder structure for new image generation

---
+++ src/bts/image2/index.h
@@ -0,0 +1,54 @@
+/*
+    Copyright 2008 Brain Research Institute, Melbourne, Australia
+
+    Written by Thomas G Close, 22/07/2012.
+
+    This file is part of MRtrix.
+
+    MRtrix is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MRtrix is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MRtrix.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef __bts_image2_index_h__
+#define __bts_image2_index_h__
+
+namespace BTS {
+
+  namespace Image2 {
+
+    class Index : public Triple<int> {
+
+      //Public static variables, nested classes and typedefs
+      public:
+
+
+      //Protected member variables
+      protected:
+
+
+      //Public member functions
+      public:
+
+
+      //Protected member functions
+      protected:
+
+
+    };
+
+  }
+
+}
+
+#endif /* __bts_image2_interpolator_h__ */
---
+++ src/bts/image2/voxel/generated.h
@@ -0,0 +1,60 @@
+/*
+    Copyright 2008 Brain Research Institute, Melbourne, Australia
+
+    Written by Thomas G Close, 22/07/2012.
+
+    This file is part of MRtrix.
+
+    MRtrix is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MRtrix is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MRtrix.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef __bts_image2_generated_h__
+#define __bts_image2_generated_h__
+
+#include "bts/image2/voxel/reference.h"
+
+namespace BTS {
+
+	namespace Image2 {
+
+    namespace Voxel {
+
+      class Generated : public Reference {
+
+        //Public static variables, nested classes and typedefs
+        public:
+
+
+        //Protected member variables
+        protected:
+
+
+        //Public member functions
+        public:
+
+
+        //Protected member functions
+        protected:
+
+
+      };
+
+    }
+
+	}
+
+}
+
+#endif /* __bts_image2_generated_h__ */
