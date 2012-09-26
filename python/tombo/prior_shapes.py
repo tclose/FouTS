32d7e95f2126eee08e538156d5dd86994ad3a795
Tom Close <tom.g.close@gmail.com>
2012-09-26 22:41:51 +0900

    Updated prior_shapes to be able to set off multiple runs

--- python/tombo/prior_shapes.py
+++ python/tombo/prior_shapes.py
@@ -17,21 +17,22 @@ import os.path
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--output_dir', default=None, type=str, help='The parent directory in which the output directory will be created (defaults to $HOME/Output)')
+parser.add_argument('--num_runs', default=1, type=int, help='The number of runs to submit to the que')
 parser.add_argument('--np', type=int, default=1, help='The the number of processes to use for the simulation (default: %(default)s)')
 parser.add_argument('--que_name', type=str, default='short', help='The the que to submit the job to(default: %(default)s)')
 args = parser.parse_args()
 
-required_dirs = [os.path.join('fibre','tract'), os.path.join('image', 'noisy')]    
+for i in xrange(args.num_runs):
+    required_dirs = [os.path.join('fibre', 'tract'), os.path.join('image', 'noisy')]
 
-# Create work directory and get path for output directory
-work_dir, output_dir = tombo.create_work_dir(SCRIPT_NAME, args.output_dir, required_dirs=required_dirs)
+    # Create work directory and get path for output directory
+    work_dir, output_dir = tombo.create_work_dir(SCRIPT_NAME, args.output_dir, required_dirs=required_dirs)
 
-# Set up command to run the script
-cmd_line = \
-"time metropolis {work_dir}/data/image/noisy/gauss20.mif \
-{work_dir}/data/fibre/tract/single/x.tct ".format(work_dir=work_dir)
+    # Set up command to run the script
+    cmd_line = "time metropolis {work_dir}/data/image/noisy/prior_background.mif \
+{work_dir}/data/fibre/tract/single/x.tct".format(work_dir=work_dir)
 
-copy_to_output= required_dirs
-# Submit job to que
-tombo.submit_job(SCRIPT_NAME, cmd_line, args.np, work_dir, output_dir, copy_to_output=copy_to_output, 
+    copy_to_output = required_dirs
+    # Submit job to que
+    tombo.submit_job(SCRIPT_NAME, cmd_line, args.np, work_dir, output_dir, copy_to_output=copy_to_output,
                                                                                 que_name=args.que_name, dry_run=True)
