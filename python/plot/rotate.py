7cb7ecd681f7c5368d8cd212134f9d26a033646d
Tom Close <tclose@oist.jp>
2012-10-03 18:27:41 +0900

    Got rotate script working as I had envisaged, might be slightly to complicated for the guys, but we will have to see.

--- python/plot/rotate.py
+++ python/plot/rotate.py
@@ -18,7 +18,6 @@ NUM_RUNS = 100
 CONF_INTERVAL = 0.99
 PLOT_COLOURS = {'Rician': 'b', 'Gaussian': 'g', 'Difference': 'r'}
 #Loop through each run for each SNR value
-fig = plt.figure()
 # Create proxy artists for legend
 plot_index_mins = []
 for snr_i, snr in enumerate(SNR_RANGE):
@@ -26,12 +25,21 @@ for snr_i, snr in enumerate(SNR_RANGE):
     for run_i in xrange(1, NUM_RUNS):
         rician = np.loadtxt(os.path.join(WORK_DIR, 'rician_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
         gauss = np.loadtxt(os.path.join(WORK_DIR, 'gaussian_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
+        # Count the number of samples
+        num_samples = len(rician)
+        assert(len(gauss) == num_samples)
+        bin_width = math.pi / float(num_samples - 1) # The width of the bins used in the scan
+        bin_width = math.pi / float(num_samples - 1) # The width of the bins used in the scan
+        # Normalise probabilities
+        rician -= np.log(np.sum(np.exp(rician)) * bin_width)
+        gauss -= np.log(np.sum(np.exp(gauss)) * bin_width)
         run_index_mins = np.minimum(rician, gauss)
         run_plot_index_mins = np.minimum(run_plot_index_mins, run_index_mins) if len(run_plot_index_mins) else run_index_mins
     plot_index_mins.append(run_plot_index_mins)
 plot_mins = [ np.min(x) for x in plot_index_mins]
 for snr_i, snr in enumerate(SNR_RANGE):
-    axes = fig.add_subplot(2, 2, snr_i + 1)
+    fig = plt.figure()
+    axes = fig.add_subplot(111) #fig.add_subplot(2, 2, snr_i + 1)
     # Calculate the minimum value for the current plot
     for run_i in xrange(1, NUM_RUNS):
         rician = np.loadtxt(os.path.join(WORK_DIR, 'rician_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
@@ -55,7 +63,7 @@ for snr_i, snr in enumerate(SNR_RANGE):
             peak_index = np.argmax(dist)
             left_index = peak_index
             right_index = peak_index
-            prob = 0.0
+            prob = math.exp(dist[peak_index]) * bin_width
             step_count = 0
             while prob < CONF_INTERVAL:
                 # Get proposed new left and right indices to potentially add to the interval
@@ -79,12 +87,12 @@ for snr_i, snr in enumerate(SNR_RANGE):
             # Calculate the angles of the the interval start and ends
             conf_intervals.append((left_index, right_index))
         rician_ci, gauss_ci = conf_intervals
-        diff_ci = (rician_ci[0] - gauss_ci[0], rician_ci[1] - gauss_ci[1])
+        diff_ci = ((rician_ci[0] - gauss_ci[0] + num_samples // 2) % num_samples, (rician_ci[1] - gauss_ci[1] + num_samples // 2) % num_samples)
         for ci, colour in zip((rician_ci, gauss_ci, diff_ci), (PLOT_COLOURS['Rician'], PLOT_COLOURS['Gaussian'],
                                                                                             PLOT_COLOURS['Difference'])):
-            for index in ci:
+            for index, style in zip(ci, ('--', ':')):
                 angle = (float(index) - float(num_samples) / 2.0) * bin_width
-                axes.plot((angle, angle), (plot_mins[snr_i], plot_index_mins[snr_i][index]), '--' + colour)
+                axes.plot((angle, angle), (plot_mins[snr_i], plot_index_mins[snr_i][index]), style + colour)
         if run_i % 100 == 0:
             print "Finished {} runs".format(run_i)
     axes.set_title('SNR {}'.format(snr))
