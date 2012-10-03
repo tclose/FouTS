d43968d7edb04d7fe6f5797a887f9fa7dd316437
Tom Close <tclose@oist.jp>
2012-10-03 11:30:08 +0900

    Trying to get .99 probability intervals imposed onto plots, but still needs some debugging

--- python/plot/rotate.py
+++ python/plot/rotate.py
@@ -16,33 +16,79 @@ FIGURE_DIR = '/home/tclose/Documents/BaFTrS/figures/'
 SNR_RANGE = [2, 5, 10, 20]
 NUM_RUNS = 100
 CONF_INTERVAL = 0.99
-BAR_INTERVAL = 0.05
-# Indices for determining the 95% confidence intervals on generated values
-low_index = round(NUM_RUNS * BAR_INTERVAL)
-high_index = round(NUM_RUNS * (1 - BAR_INTERVAL))
-conf_intervals = []
+PLOT_COLOURS = {'Rician': 'b', 'Gaussian': 'g', 'Difference': 'r'}
 #Loop through each run for each SNR value
 fig = plt.figure()
+# Create proxy artists for legend
+plot_index_mins = []
 for snr_i, snr in enumerate(SNR_RANGE):
-    axes = fig.add_subplot(2, 2, snr_i + 1)
+    run_plot_index_mins = [] # The minimums for each index of every plot
     for run_i in xrange(1, NUM_RUNS):
         rician = np.loadtxt(os.path.join(WORK_DIR, 'rician_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
         gauss = np.loadtxt(os.path.join(WORK_DIR, 'gaussian_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
-        assert(len(rician) == len(gauss))
+        run_index_mins = np.minimum(rician, gauss)
+        run_plot_index_mins = np.minimum(run_plot_index_mins, run_index_mins) if len(run_plot_index_mins) else run_index_mins
+    plot_index_mins.append(run_plot_index_mins)
+plot_mins = [ np.min(x) for x in plot_index_mins]
+for snr_i, snr in enumerate(SNR_RANGE):
+    axes = fig.add_subplot(2, 2, snr_i + 1)
+    # Calculate the minimum value for the current plot
+    for run_i in xrange(1, NUM_RUNS):
+        rician = np.loadtxt(os.path.join(WORK_DIR, 'rician_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
+        gauss = np.loadtxt(os.path.join(WORK_DIR, 'gaussian_snr{snr}_run{run_i}.txt'.format(snr=snr, run_i=run_i)), comments='%')[:, 0]
+        # Count the number of samples
+        num_samples = len(rician)
+        assert(len(gauss) == num_samples)
+        bin_width = math.pi / float(num_samples - 1) # The width of the bins used in the scan
         # Normalise probabilities
-        rician -= np.log(np.sum(np.exp(rician)))
-        gauss -= np.log(np.sum(np.exp(gauss)))
+        rician -= np.log(np.sum(np.exp(rician)) * bin_width)
+        gauss -= np.log(np.sum(np.exp(gauss)) * bin_width)
+        angles = np.arange(-math.pi / 2.0, math.pi / 2.0, bin_width)
+        # Plot the differences
         diff = rician - gauss
-        angles = np.arange(-math.pi / 2, math.pi / 2, math.pi / (len(rician) - 1))
-        axes.plot(angles, rician, 'b')
-        axes.plot(angles, gauss, 'r')
-        axes.plot(angles, diff, 'g')
-#        ninety_five = find(rician_scan > log(1 - conf_interval));
-#        min_x = x_axis(min(ninety_five));
-#        max_x = x_axis(max(ninety_five));
-#        conf_intervals(run_i, snr_i) = max_x - min_x;
+        axes.plot(angles, rician, PLOT_COLOURS['Rician'])
+        axes.plot(angles, gauss, PLOT_COLOURS['Gaussian'])
+        axes.plot(angles, diff, PLOT_COLOURS['Difference'])
+        # Calculate the confidence intervals
+        conf_intervals = []
+        for dist in (gauss, rician):
+            peak_index = np.argmax(dist)
+            left_index = peak_index
+            right_index = peak_index
+            prob = 0.0
+            step_count = 0
+            while prob < CONF_INTERVAL:
+                # Get proposed new left and right indices to potentially add to the interval
+                prop_left_index = (left_index - 1) % num_samples
+                prop_right_index = (right_index + 1) % num_samples
+                # Get the values at the proposed indices
+                left_val = math.exp(dist[prop_left_index])
+                right_val = math.exp(dist[prop_right_index])
+                # Add the index that will increase the probability the most to the interval
+                if left_val > right_val:
+                    prob += left_val * bin_width
+                    left_index = prop_left_index
+                else:
+                    prob += right_val * bin_width
+                    right_index = prop_right_index
+                step_count += 1
+                if step_count > num_samples:
+                    left_index = 0
+                    right_index = num_samples - 1
+                    break
+            # Calculate the angles of the the interval start and ends
+            conf_intervals.append((left_index, right_index))
+        rician_ci, gauss_ci = conf_intervals
+        diff_ci = (rician_ci[0] - gauss_ci[0], rician_ci[1] - gauss_ci[1])
+        for ci, colour in zip((rician_ci, gauss_ci, diff_ci), (PLOT_COLOURS['Rician'], PLOT_COLOURS['Gaussian'],
+                                                                                            PLOT_COLOURS['Difference'])):
+            for index in ci:
+                angle = (float(index) - float(num_samples) / 2.0) * bin_width
+                axes.plot((angle, angle), (plot_mins[snr_i], plot_index_mins[snr_i][index]), '--' + colour)
         if run_i % 100 == 0:
             print "Finished {} runs".format(run_i)
     axes.set_title('SNR {}'.format(snr))
+    axes.set_xlabel('Rotation (Radians)')
+    axes.set_ylabel('Probability')
     print "Finished snr {}".format(snr)
 plt.show()
