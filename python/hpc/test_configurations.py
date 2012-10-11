1ab701a22304984e7ec8d4d93bc5fc6de13bccc3
Tom Close <tclose@oist.jp>
2012-10-11 19:17:33 +0900

    changed test_configurations to use density of 1.0 by default instead of acs of 1

--- python/hpc/test_configurations.py
+++ python/hpc/test_configurations.py
@@ -148,7 +148,7 @@ img_dims "{img_dim} {img_dim} {img_dim}" -exp_type {args.interp_type} \
 
 # Initialise fibres
 init_fibres {work_dir}/output/init.tct -num_fibres {num_tracts} \
--img_dims "{img_dim} {img_dim} {img_dim}" -degree {args.degree} -seed {init_seed} -acs 1.0 -base_intensity 1.0
+-img_dims "{img_dim} {img_dim} {img_dim}" -degree {args.degree} -seed {init_seed} -base_intensity 1.0
 
 # Run metropolis
 metropolis {work_dir}/output/image.mif {work_dir}/output/init.tct {work_dir}/output/samples.tst -like_snr {like_snr} \
