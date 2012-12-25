9f5adb20581636ab188865fccc32f1ab52f98d3b
Tom Close <tclose@oist.jp>
2012-12-25 15:48:40 +0900

    switched back to inital tract configurations

--- python/hpc/baftrs_in_action.py
+++ python/hpc/baftrs_in_action.py
@@ -20,7 +20,6 @@ INIT_CONFIGS = [os.path.join('donald', 'init1.tct'),
                 os.path.join('donald', 'init3.tct'),
                 os.path.join('donald', 'init4.tct'),
                 os.path.join('donald', 'init5.tct')]
-INIT_CONFIGS = [os.path.join('donald', 'rand_init.tct')]
 #DATASETS = [os.path.join('donald', 'fornix.mif'), os.path.join('heath', 'fornix.mif'), os.path.join('lisa', 'fornix.mif')]
 DATASETS = [os.path.join('donald', 'fornix.mif')] * len(INIT_CONFIGS)
 # Required dirs for the script to run
