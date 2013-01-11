3e0c17d57d44db783cc33d01702f0185d8dbc736
Tom Close <tclose@oist.jp>
2013-01-11 11:05:36 +0900

    Started calculate_num_samples (hopefully finish this evening)

---
+++ cmd/calculate_num_samples.cpp
@@ -0,0 +1,132 @@
+/*
+ Copyright 2009 Brain Research Institute, Melbourne, Australia
+
+ Written by Thomas G. Close, 04/03/2009.
+
+ This file is part of Bayesian Tractlet Sampling (BTS).
+
+ BTS is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ BTS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with BTS.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
+#include "bts/cmd.h"
+
+#include "bts/common.h"
+
+
+#include "bts/fibre/strand/set.h"
+
+
+#include "bts/fibre/strand/set.h"
+#include "bts/fibre/track/set.h"
+#include "bts/fibre/tractlet/set.h"
+#include "bts/fibre/tractlet/section.h"
+#include "bts/fibre/base/set_reader.h"
+#include "bts/fibre/base/set_writer.h"
+
+
+#include "bts/inline_functions.h"
+
+using namespace BTS;
+
+const float SAMPLES_PER_ACS_DEFAULT = 1000;
+const float SAMPLES_PER_LENGTH_DEFAULT = 1000;
+const size_t MIN_WIDTH_SAMPLES_DEFAULT = 4;
+const size_t MIN_LENGTH_SAMPLES_DEFAULT = 4;
+const char* STRATEGY_DEFAULT = "max";
+
+
+SET_VERSION_DEFAULT;
+SET_AUTHOR ("Thomas G. Close");
+SET_COPYRIGHT (NULL);
+
+DESCRIPTION = {
+  "Print strands to screen",
+  "",
+  NULL
+};
+
+ARGUMENTS = {
+  Argument ("input", "The strands to be displayed.").type_file (),
+  Argument ("output", "The prefix for the outputted files (the two output files will be appended with 'length.txt' and "
+                      "'width.txt' respectively.").type_file (),
+  Argument()
+};
+
+
+OPTIONS = {
+
+    Option ("samples_per_acs","")
+     + Argument ("samples_per_acs","").type_integer (0,SAMPLES_PER_ACS_DEFAULT,LARGE_FLOAT),
+
+    Option ("samples_per_length","")
+     + Argument ("samples_per_length","").type_integer (0,SAMPLES_PER_LENGTH_DEFAULT,LARGE_FLOAT),
+
+    Option ("min_length_samples","")
+     + Argument ("min_length_samples","").type_integer (1,MIN_LENGTH_SAMPLES_DEFAULT,LARGE_INT),
+
+    Option ("min_width_samples","")
+     + Argument ("min_width_samples","").type_integer (1,MIN_WIDTH_SAMPLES_DEFAULT,LARGE_INT),
+
+    Option ("strategy","The strategy used to handle multiple Fourier tracts.")
+     + Argument ("strategy","").type_text(STRATEGY_DEFAULT),
+Option() };
+
+
+
+EXECUTE {
+
+  std::string input_location = argument[0];
+  std::string output_prefix = argument[1];
+
+  double samples_per_acs  = SAMPLES_PER_ACS_DEFAULT;
+  double samples_per_length  = SAMPLES_PER_LENGTH_DEFAULT;
+  size_t min_width_sections  = MIN_WIDTH_SAMPLES_DEFAULT;
+  size_t min_length_sections  = MIN_LENGTH_SAMPLES_DEFAULT;
+  std::string strategy = STRATEGY_DEFAULT;
+
+  Options opt = get_options("samples_per_acs");
+  if (opt.size())
+    samples_per_acs = opt[0][0];
+
+  Options opt = get_options("samples_per_length");
+  if (opt.size())
+    samples_per_length = opt[0][0];
+
+  Options opt = get_options("min_width_sections");
+  if (opt.size())
+    min_width_sections = opt[0][0];
+
+  Options opt = get_options("min_length_sections");
+  if (opt.size())
+    min_length_sections = opt[0][0];
+
+  Options opt = get_options("strategy");
+  if (opt.size())
+    strategy = opt[0][0];
+  
+  Fibre::Tractlet::Set tcts(input_location);
+
+  std::vector<double> acs_vector;
+  std::vector<double> length_vector;
+
+  for (size_t tract_i = 0; tract_i < tcts.size(); ++tract_i) {
+    acs_vector.push_back(tcts[tract_i].acs());
+    length_vector.push_back(tcts[tract_i].path_length());
+  }
+
+  // Do length max and acs max/average calculations here
+
+}
