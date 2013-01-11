482cc940a8202c7f1a018b07780fe5eb40e0671d
Tom Close <tclose@oist.jp>
2013-01-11 10:43:22 +0900

    Updated baftrs_in_action to run multiple iterations with normalised number of samples

--- python/hpc/baftrs_in_action.py
+++ python/hpc/baftrs_in_action.py
@@ -14,6 +14,7 @@ import time
 #Name of the script for the output directory and submitted mpi job
 SCRIPT_NAME = 'baftrs_in_action'
 DATASETS = [os.path.join('donald', 'fornix.mif')]
+INIT_LOCATIONS = [((111, 119, 73), (111, 80, 75))]
 #DATASETS = [os.path.join('donald', 'fornix.mif'), os.path.join('heath', 'fornix.mif'), os.path.join('lisa', 'fornix.mif')]
 # Required dirs for the script to run
 REQUIRED_DIRS = ['params/image/reference', 'params/diffusion', 'params/fibre/tract/masks/mcmc/metropolis']
@@ -23,8 +24,10 @@ parser.add_argument('--step_scale', default=0.015, type=float, help='The scale o
 parser.add_argument('--num_iterations', default=100000, type=int, help='The number of interations in the metropolis sampling (default: %(default)s)')
 parser.add_argument('--sample_period', default=1000, type=int, help='The sample period of the metropolis sampling (default: %(default)s)')
 parser.add_argument('--num_norms', type=int, default=3, help='The number of times the num_width_sections and num_length_sections are normalised (default: %(default)s)')
-parser.add_argument('--strands_per_acs', type=float, default=100, help='The number of strands to use per ACS when renormalising the tractlets at the start of each major iteration (default: %(default)s)')
+parser.add_argument('--samples_per_acs', type=float, default=100, help='The number of strands to use per ACS when renormalising the tractlets at the start of each major iteration (default: %(default)s)')
 parser.add_argument('--samples_per_length', type=float, default=100, help='The number of length samples to use per ACS when renormalising the tractlets at the start of each major iteration (default: %(default)s)')
+parser.add_argument('--min_length_samples', type=float, default=15, help='The number of strands to use per ACS when renormalising the tractlets at the start of each major iteration (default: %(default)s)')
+parser.add_argument('--min_width_samples', type=float, default=4, help='The number of length samples to use per ACS when renormalising the tractlets at the start of each major iteration (default: %(default)s)')
 parser.add_argument('--interp_type', default='sinc', type=str, help='The type of interpolation used in the reference image (default: %(default)s)')
 parser.add_argument('--interp_extent', default=1, type=int, help='The interpolation extent used in the reference image (default: %(default)s)')
 parser.add_argument('--assumed_interp_extent', default=1, type=int, help='The interpolation type used in the likelihood images (default: %(default)s)')
@@ -40,9 +43,7 @@ parser.add_argument('--length_epsilon', default=0.15, type=float, help='The amou
 parser.add_argument('--img_snr', default=20.0, type=float, help='The snr to used in the noisy image')
 parser.add_argument('--like_snr', default=[20.0], type=float, nargs='+',
                     help='The assumed snr to used in the likelihood function in the metropolis sampling')
-parser.add_argument('--init_offset', default=(115, 117, 75), nargs=3, type=float,
-                    help='The offset of the ROI in which the inital tracts are generated (default: %(default)s)')
-parser.add_argument('--init_extent', default=(10, 10, 10), nargs=3, type=int,
+parser.add_argument('--init_extent', default=(1, 1, 1), nargs=3, type=int,
                     help='The extent (in number of voxels) of the ROI in which the inital tracts are generated')
 parser.add_argument('--seed', type=int, help="The random seed for the whole run")
 parser.add_argument('--output_dir', default=None, type=str, help='The parent directory in which the output directory \
@@ -74,7 +75,7 @@ for i in xrange(args.num_runs):
     for prior_freq, prior_aux_freq, prior_density_low, prior_density_high, prior_hook, prior_thin, like_snr, \
                                                                 width_epsilon, length_epsilon in zip(*ranging_params):
 
-        for dataset in DATASETS:
+        for dataset, init_locations in zip(DATASETS, INIT_LOCATIONS):
             # Create work directory and get path for output directory
             work_dir, output_dir = hpc.create_work_dir(SCRIPT_NAME, args.output_dir, required_dirs=REQUIRED_DIRS)
             with open(os.path.join(work_dir, 'summary.txt'), 'w') as f:
@@ -95,37 +96,62 @@ for i in xrange(args.num_runs):
             else:
                 response_str = ""
             response_b0_path = os.path.join(work_dir, 'params', 'image', 'reference', dataset_dir, 'response.b0.txt')
-            cmd_line = """             
+            cmd_line = """
+            # Initialise empty set to hold the initial locations of the tractlets
+            new_fibres -set_size 0 -acs 1.0 {work_dir}/output/init_0.tct
+            
+"""
+            if not len(init_locations):
+                raise Exception("Initial locations list was empty")
+            for i, init_location in enumerate(init_locations):
+                cmd_line += """             
 # Create initial_fibres of appropriate degree
-init_fibres {work_dir}/output/init.tct -degree {args.degree} -num_fibres 1 -width_epsilon {args.width_epsilon} \
+init_fibres {work_dir}/init_location_{i}.tct -degree {args.degree} -num_fibres 1 -width_epsilon {args.width_epsilon} \
 -length_epsilon {args.length_epsilon} -edge_buffer 0.0 -seed {init_seed} -base_intensity 1.0 
 -img_dims "{init_extent[0]} {init_extent[1]} {init_extent[2]}" \
--img_offset "{init_offset[0]} {init_offset[1]} {init_offset[2]}" 
+-img_offset "{init_location[0]} {init_location[1]} {init_location[2]}"
 
-cp {work_dir}/output/init.tct {work_dir}/tracts.tct
-cp {work_dir}/output/init.tctx {work_dir}/tracts.tctx
-"""
+# Add new tract to initial set
+combine_fibres {work_dir}/output/init_0.tct {work_dir}/init_location_{i}.tct {work_dir}/tmp.tct
+mv {work_dir}/tmp.tct {work_dir}/output/init_0.tct
+mv {work_dir}/tmp.tctx {work_dir}/output/init_0.tct
+
+""".format(work_dir=work_dir, i=i, init_location=init_location, init_extent=args.init_extent)
+            # Set required properties and normalise the density of the initial tracts
+            cmd_line += """
+# Set required properties
+set_property {work_dir}/output/init_0.tct -set length_epsilon {args.length_epsilon} \
+-set width_epsilon {args.width_epsilon} -set base_intensity 1.0
+
+# Normalise the density of the initial tracts
+normalise_density {work_dir}/output/init_0.tct
+
+""".format(work_dir=work_dir, args=args)
+
             for norm_count in range(args.num_norms):
                 cmd_line += """
+                
 # Calculate appropriate number of length and width samples                
-calculate_num_samples -strands_per_acs {args.strands_per_acs} -samples_per_length {args.samples_per_length} \
-{work_dir}/tracts.tct {work_dir}/num_samples
+calculate_num_samples --samples_per_acs {args.samples_per_acs} --samples_per_length {args.samples_per_length} \
+--strategy max {work_dir}/init_{norm_count}.tct {work_dir}/num_samples --min_length_samples {args.min_length_samples} \
+--min_width_samples {args.min_width_samples}
                 
 # Run metropolis
-metropolis {dataset_path} {work_dir}/output/init.tct {work_dir}/output/samples.tst -like_snr {like_snr} \
--exp_interp_extent {args.assumed_interp_extent} -walk_step_scale {args.step_scale} -num_iter {args.num_iterations} \
--sample_period {args.sample_period} -diff_encodings_location {work_dir}/params/diffusion/encoding_60.b \
--seed {seed} -prior_freq {prior_freq} {prior_aux_freq} -prior_density {prior_density_high} \
-{prior_density_low} 100 -prior_hook {prior_hook} 100 15 -prior_thin {prior_thin} 2 \
--exp_num_width_sections {args.num_width_sections} -exp_type {args.interp_type} {response_str} \
--exp_b0 `cat {b0_path}` -diff_warn -walk_step_location \
+metropolis {dataset_path} {work_dir}/output/init_{norm_count}.tct {work_dir}/output/samples_{norm_count}.tst \
+-like_snr {like_snr} -exp_interp_extent {args.assumed_interp_extent} -walk_step_scale {args.step_scale} \
+-num_iter {args.num_iterations} -sample_period {args.sample_period} \
+-diff_encodings_location {work_dir}/params/diffusion/encoding_60.b -seed {seed} \
+-prior_freq {prior_freq} {prior_aux_freq} -prior_density {prior_density_high} {prior_density_low} 100 \
+-prior_hook {prior_hook} 100 15 -prior_thin {prior_thin} 2 \
+-exp_num_length_sections `cat {work_dir}/num_samples.length.txt` \
+-exp_num_width_sections `cat {work_dir}/num_samples.width.txt` \
+-exp_type {args.interp_type} {response_str} -exp_b0 `cat {b0_path}` -diff_warn -walk_step_location \
 {work_dir}/params/fibre/tract/masks/mcmc/metropolis/default{args.degree}.tct
-
     """.format(work_dir=work_dir, dataset_path=dataset_path, args=args,
-               seed=seed, init_seed=seed + 1, prior_freq=prior_freq, prior_aux_freq=prior_aux_freq,
-               prior_density_low=prior_density_low, prior_density_high=prior_density_high, prior_hook=prior_hook,
-               prior_thin=prior_thin, like_snr=like_snr, response_str=response_str, b0_path=response_b0_path,
-               init_extent=args.init_extent, init_offset=args.init_offset, norm_count=norm_count)
+                    seed=seed, init_seed=seed + 1, prior_freq=prior_freq, prior_aux_freq=prior_aux_freq,
+                    prior_density_low=prior_density_low, prior_density_high=prior_density_high, prior_hook=prior_hook,
+                    prior_thin=prior_thin, like_snr=like_snr, response_str=response_str, b0_path=response_b0_path,
+                    norm_count=norm_count)
             # Submit job to que
             hpc.submit_job(SCRIPT_NAME, cmd_line, args.np, work_dir, output_dir, que_name=args.que_name,
                                                                 dry_run=args.dry_run, copy_to_output=['summary.txt'])
