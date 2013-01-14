4c33e7eb5d40e40a0ab09ff0e4a892abc7e4c03d
Tom Close <tclose@oist.jp>
2013-01-15 00:17:47 +0900

    added original image transform to select_image

--- cmd/select_image.cpp
+++ cmd/select_image.cpp
@@ -20,7 +20,6 @@
 
  */
 
-
 extern "C" {
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
@@ -45,10 +44,10 @@ extern "C" {
 #include "bts/inline_functions.h"
 
 using namespace BTS;
-
-SET_VERSION_DEFAULT;
-SET_AUTHOR ("Thomas G. Close");
-SET_COPYRIGHT (NULL);
+SET_VERSION_DEFAULT
+;
+SET_AUTHOR("Thomas G. Close");
+SET_COPYRIGHT(NULL);
 
 DESCRIPTION = {
   "Selects a rectangular region of interest from a larger image.",
@@ -56,7 +55,7 @@ DESCRIPTION = {
   NULL
 };
 
-ARGUMENTS = {
+ARGUMENTS= {
   Argument ("initial", "The initial image the second image will be subtracted from.").type_image_in(),
 
   Argument ("output_image", "The resulting image").type_file (),
@@ -64,94 +63,96 @@ ARGUMENTS = {
   Argument()
 };
 
-
-OPTIONS = {
+OPTIONS= {
 
   Option ("dims", "The number of voxels along each dimension")
-   + Argument ("dims", "").type_text ("[3,3,3]"),
+  + Argument ("dims", "").type_text ("[3,3,3]"),
 
   Option ("offsets", "The offset of the centre of the image from the origin (0,0,0).")
-   + Argument ("offsets", "").type_text ("auto"),
+  + Argument ("offsets", "").type_text ("auto"),
 
   Option("remove_isotropic", "Removes the isotropic component of the signal (assumes that the signal is acquired at a \
       constant b value"),
 
-Option()
+  Option()
 
 };
 
-
-
 EXECUTE {
 
-  Triple<size_t>    dims (3,3,3);
-  Triple<size_t>    offsets (0,0,0);
-  bool remove_isotropic = false;
+    Triple<size_t> dims(3, 3, 3);
+    Triple<size_t> offsets(0, 0, 0);
+    bool remove_isotropic = false;
 
-  Options opt = get_options("dims");
-  if (opt.size())
-    dims = parse_triple<size_t>(std::string(opt[0][0]));
+    Options opt = get_options("dims");
+    if (opt.size())
+      dims = parse_triple<size_t>(std::string(opt[0][0]));
 
-  opt = get_options("offsets");
-  if (opt.size())
-    offsets = parse_triple<size_t>(std::string(opt[0][0]));
+    opt = get_options("offsets");
+    if (opt.size())
+      offsets = parse_triple<size_t>(std::string(opt[0][0]));
 
-  opt = get_options("remove_isotropic");
-  if (opt.size())
-    remove_isotropic = true;
+    opt = get_options("remove_isotropic");
+    if (opt.size())
+      remove_isotropic = true;
 
-  MR::Image::Header in (argument[0]);
-  std::string output_location   = argument[1];
+    MR::Image::Header in(argument[0]);
+    std::string output_location = argument[1];
 
-  Diffusion::Encoding::Set dw_scheme(in.get_DW_scheme());
+    Diffusion::Encoding::Set dw_scheme(in.get_DW_scheme());
 
-  Triple<double> spatial_offset(in.vox(X) * offsets[X], in.vox(Y) * offsets[Y], in.vox(Z) * offsets[Z]);
+    Triple<double> spatial_offset(in.vox(X) * offsets[X] + in.transform()(X,3),
+        in.vox(Y) * offsets[Y] + in.transform()(Y,3),
+        in.vox(Z) * offsets[Z] + in.transform()(Z,3));
 
-  Image::Observed::Buffer out(dims, Triple<double>(in.vox(X), in.vox(Y), in.vox(Z)), spatial_offset, dw_scheme);
+    Image::Observed::Buffer out(dims,
+        Triple<double>(in.vox(X), in.vox(Y), in.vox(Z)), spatial_offset,
+        dw_scheme);
 
-  size_t dim_array[] = {dims[X], dims[Y], dims[Z], dw_scheme.num_encodings()};
-  size_t offset_array[] = {offsets[X], offsets[Y], offsets[Z], 0};
+    size_t dim_array[] =
+        { dims[X], dims[Y], dims[Z], dw_scheme.num_encodings() };
+    size_t offset_array[] = { offsets[X], offsets[Y], offsets[Z], 0 };
 
-  MR::Image::Voxel<float> vox (in);
-  MR::DataSet::Subset<MR::Image::Voxel<float> > vox_subset(vox, offset_array, dim_array);
+    MR::Image::Voxel<float> vox(in);
+    MR::DataSet::Subset<MR::Image::Voxel<float> > vox_subset(vox, offset_array,
+        dim_array);
 
-  MR::ProgressBar progress_bar ("Selecting subset of image...", dw_scheme.num_encodings());
-  MR::DataSet::Loop loop (0,4);
-  size_t x, y, z, encoding_i;
-  x = y = z = encoding_i = 0;
-  for (loop.start (vox_subset); loop.ok(); loop.next (vox_subset)) {
-    if (x >= dims[X]) {
-      x = 0;
-      ++y;
-      if (y >= dims[Y]) {
-        y = 0;
-        ++z;
-        if (z >= dims[Z]) {
-          z = 0;
-          ++encoding_i;
-          ++progress_bar;
-          if (encoding_i >= dw_scheme.num_encodings())
-            throw Exception("Incremented past the end of the number of encodings dimension, something has gone wrong.");
+    MR::ProgressBar progress_bar("Selecting subset of image...",
+        dw_scheme.num_encodings());
+    MR::DataSet::Loop loop(0, 4);
+    size_t x, y, z, encoding_i;
+    x = y = z = encoding_i = 0;
+    for (loop.start(vox_subset); loop.ok(); loop.next(vox_subset)) {
+      if (x >= dims[X]) {
+        x = 0;
+        ++y;
+        if (y >= dims[Y]) {
+          y = 0;
+          ++z;
+          if (z >= dims[Z]) {
+            z = 0;
+            ++encoding_i;
+            ++progress_bar;
+            if (encoding_i >= dw_scheme.num_encodings())
+              throw Exception("Incremented past the end of the number of "
+                              "encodings dimension, something has gone wrong.");
+          }
         }
       }
+      out(x, y, z)[encoding_i] = (float) vox_subset.value();
+      ++x;
     }
-    out(x,y,z)[encoding_i] = (float)vox_subset.value();
-    ++x;
-  }
 
-  out.properties()["original_image"] = argument[0].c_str();
-  out.properties()["offsets"] = str(offsets);
+    out.properties()["original_image"] = argument[0].c_str();
+    out.properties()["offset_from_original"] = str(offsets);
 
-  if (remove_isotropic)
-    out.remove_isotropic();
+    if (remove_isotropic)
+      out.remove_isotropic();
 
 //-------------//
 //  Save Image //
 //-------------//
-  out.save(output_location);
+    out.save(output_location);
 
-
-
-}
-
+  }
 
