8d29497f40d9db64c087a5e4c69526e691e0c304
Tom Close <tclose@oist.jp>
2013-04-14 11:04:22 +0900

    creating objects to stored image and tract data

--- python/image.py
+++ python/image.py
@@ -1,6 +1,6 @@
 # Copyright 2013 Florey Neuroscience Institutes
 #
-# Created by Tom Close on 13/03/09.
+# maked by Tom Close on 13/03/09.
 #
 # This file is part of the Fourier Tract Sampling (FouTS) package.
 #
@@ -17,79 +17,160 @@
 # You should have received a copy of the GNU General Public License
 # along with FouTS.  If not, see <http://www.gnu.org/licenses/>.
 
+import collections
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-def create_along_samples(degree, num_samples, include_endpoints=False):
-    # Get time increments minus endpoints
-    t = np.linspace(0, 1, num_samples + 2)
-    if not include_endpoints:
-        t = t[1:-1]
-    psi = np.ones((num_samples, degree))
-    for d in xrange(1, degree):
-        psi[:, d] = np.sqrt(2) * np.cos(d * np.pi * t)
-    return psi
+NumberOfSamples = collections.namedtuple("NumberOfSamples", "longitudinal radial")
 
-def create_tangent_samples(degree, num_samples):
-    # Get time increments minus endpoints
-    t = np.linspace(0, 1, num_samples + 2)[1:-1] # Remove endpoints which are always [0,0,0]
-    dpsi_dt = np.zeros((num_samples, degree))
-    for d in xrange(1, degree):
-        dpsi_dt[:, d] = -np.sqrt(2) * np.pi * np.sin(d * np.pi * t)
-    return dpsi_dt
+class Image(object):
 
-def create_across_samples(num_samples):
-    """
-    Creates pairs of fractions that hexagonally pack the unit circle with a maximal number of 
-    samples along its radius of 2 * num_samples + 1
-    """
-    sub_radius = 1.0 / float(2 * num_samples + 1)
-    incr1 = sub_radius * 2.0
-    incr2 = sub_radius * np.sqrt(3)
-    samples = []
-    offset_row = False
-    for step2 in np.arange(0, 1, incr2):
-        offset = sub_radius if offset_row else 0.0
-        for k2 in ((-step2, step2) if step2 else (0,)):
-            for step1 in (np.arange(0, 1, incr1) + offset):
-                for k1 in ((-step1, step1) if step1 else (0,)):
-                    if k1 ** 2 + k2 ** 2 <= (1.0 - sub_radius):
-                        samples.append((k1, k2))
-        offset_row = not offset_row
-    return samples
+    def __init__(self, size, resolution, encodings):
+        if len(size) != 3 or len(resolution) != 3:
+            raise Exception("Image must be three-dimensional (provided: size={}, resolution={})."
+                            .format(size, resolution))
+        if encodings.shape[1] != 4:
+            raise Exception("The second dimension of the encodings matrix must have length 4 (found"
+                            " {}) corresponding to [x y z b_value] ".format(encodings.shape[1]))
+        self._size = size
+        self._resolution = resolution
+        self._encodings = encodings
+        self._data = np.zeros((np.prod(size), encodings.shape[0]))
 
-def create_samples(degree, num_along_samples, num_across_samples):
+    # These are provided as read only versions of the 
+    @property
+    def size(self):
+        return self._size
 
-    along_samples = create_along_samples(degree, num_along_samples)
-    across_samples = create_across_samples(num_across_samples)
-    samples = np.zeros((num_along_samples * len(across_samples), degree * 3))
-    for i, (k1, k2) in enumerate(across_samples):
-        row_start = i * num_along_samples
-        row_end = row_start + num_along_samples
-        samples[row_start:row_end, :degree] = along_samples
-        samples[row_start:row_end, degree:(degree * 2)] = along_samples * k1
-        samples[row_start:row_end, (degree * 2):(degree * 3)] = along_samples * k2
-    return samples
+    @property
+    def resolution(self):
+        return self._resolution
 
-def create_tangents(degree, num_tangent_samples, num_across_samples):
+    @property
+    def encodings(self):
+        return self._encodings
 
-    tangent_samples = create_tangent_samples(degree, num_tangent_samples)
-    across_samples = create_across_samples(num_across_samples)
-    samples = np.zeros((num_tangent_samples * len(across_samples), degree * 3))
-    for i, (k1, k2) in enumerate(across_samples):
-        row_start = i * num_tangent_samples
-        row_end = row_start + num_tangent_samples
-        samples[row_start:row_end, :degree] = tangent_samples
-        samples[row_start:row_end, degree:(degree * 2)] = tangent_samples * k1
-        samples[row_start:row_end, (degree * 2):(degree * 3)] = tangent_samples * k2
-    return samples
+    def generate(self, tract, N_longitudinal, N_radial, response_coeffs):
+        samples_matrix = Tract.position_matrix(len(tract), N_longitudinal, N_radial)
+        tangents_matrix = Tract.tangent_matrix(len(tract), N_longitudinal, N_radial)
+        positions = samples_matrix.dot(tract)
+        tangents = tangents_matrix.dot(tract)
+        lengths = np.sqrt(tangents[:, 0] ** 2 + tangents[:, 1] ** 2 + tangents[:, 2] ** 2)
+
+        indices = np.floor(positions)
+        remain = positions - indices
+        disps = ([], [], [])
+        for d in xrange(3):
+            for i in xrange((1 - INTERP_LENGTH), INTERP_LENGTH):
+                disps[d].append(remain[:, d] - i)
+
+
+class Tract(object):
+
+    _longitudinal_position_matrices = {}
+    _longitudinal_tangent_matrices = {}
+    _radial_matrices = {}
+    _position_matrices = {}
+    _tangent_matrices = {}
+
+    @classmethod
+    def position_matrix(cls, degree, N_longitudinal, N_radial):
+        N = NumberOfSamples(N_longitudinal, N_radial)
+        try:
+            matrix = cls._position_matrices[(degree, N)]
+        except KeyError:
+            try:
+                long_matrix = cls._longitudinal_position_matrices[(degree, N.longitudinal)]
+            except KeyError:
+                long_matrix = cls._make_longitudinal_matrix(degree, N)
+                cls._longitudinal_position_matrices[(degree, N.longitudinal)] = long_matrix
+            try:
+                radial_matrix = cls._radial_matrices[(degree, N.radial)]
+            except KeyError:
+                radial_matrix = cls._make_radial_matrix(degree, N)
+                cls._radial_matrices[(degree, N.raial)] = radial_matrix
+            matrix = cls._combine_sample_matrices(long_matrix, radial_matrix)
+        return matrix
+
+    @classmethod
+    def tangent_matrix(cls, degree, N_longitudinal, N_radial):
+        N = NumberOfSamples(N_longitudinal, N_radial)
+        try:
+            matrix = cls._tangent_matrices[(degree, N)]
+        except KeyError:
+            try:
+                long_matrix = cls._longitudinal_tangent_matrices[(degree, N.longitudinal)]
+            except KeyError:
+                long_matrix = cls._make_tangent_matrix(degree, N)
+                cls._longitudinal_tangent_matrices[(degree, N.longitudinal)] = long_matrix
+            try:
+                radial_matrix = cls._radial_matrices[(degree, N.radial)]
+            except KeyError:
+                radial_matrix = cls._make_radial_matrix(degree, N)
+                cls._radial_matrices[(degree, N.raial)] = radial_matrix
+            matrix = cls._combine_sample_matrices(long_matrix, radial_matrix)
+        return matrix
+
+    @classmethod
+    def _make_longitudinal_matrix(cls, degree, N, include_endpoints=False):
+        # Get time increments minus endpoints
+        t = np.linspace(0, 1, N + 2)
+        if not include_endpoints:
+            t = t[1:-1]
+        psi = np.ones((N, degree))
+        for d in xrange(1, degree):
+            psi[:, d] = np.sqrt(2) * np.cos(d * np.pi * t)
+        return psi
+
+    @classmethod
+    def _make_tangents_matrix(cls, degree, N):
+        # Get time increments minus endpoints
+        t = np.linspace(0, 1, N + 2)[1:-1] # Remove endpoints which are always [0,0,0]
+        dpsi_dt = np.zeros((N, degree))
+        for d in xrange(1, degree):
+            dpsi_dt[:, d] = -np.sqrt(2) * np.pi * np.sin(d * np.pi * t)
+        return dpsi_dt
+
+    @classmethod
+    def _make_radial_matrix(cls, N):
+        """
+        makes pairs of fractions that hexagonally pack the unit circle with a maximal number of 
+        samples along its radius of 2 * num_samples + 1
+        """
+        sub_radius = 1.0 / float(2 * N + 1)
+        incr1 = sub_radius * 2.0
+        incr2 = sub_radius * np.sqrt(3)
+        samples = []
+        offset_row = False
+        for step2 in np.arange(0, 1, incr2):
+            offset = sub_radius if offset_row else 0.0
+            for k2 in ((-step2, step2) if step2 else (0,)):
+                for step1 in (np.arange(0, 1, incr1) + offset):
+                    for k1 in ((-step1, step1) if step1 else (0,)):
+                        if k1 ** 2 + k2 ** 2 <= (1.0 - sub_radius):
+                            samples.append((k1, k2))
+            offset_row = not offset_row
+        return samples
+
+    @classmethod
+    def _combine_sample__matrices(cls, longitudinal_matrix, radial_matrix):
+        length = longitudinal_matrix.shape[0]
+        degree = longitudinal_matrix.shape[1]
+        samples = np.zeros((length * len(radial_matrix), degree * 3))
+        for i, (k1, k2) in enumerate(radial_matrix):
+            row_start = i * length
+            row_end = row_start + length
+            samples[row_start:row_end, :degree] = longitudinal_matrix
+            samples[row_start:row_end, degree:(degree * 2)] = longitudinal_matrix * k1
+            samples[row_start:row_end, (degree * 2):(degree * 3)] = longitudinal_matrix * k2
+        return samples
 
 if __name__ == '__main__':
 
     DEGREE = 4
     NUM_LENGTH_SAMPLES = 25
-    NUM_ACROSS_SAMPLES = 4
+    NUM_RADIAL = 4
     INTERP_LENGTH = (1, 1, 1)
 
     tract = np.zeros((DEGREE * 3, 3))
@@ -98,19 +179,10 @@ if __name__ == '__main__':
     tract[DEGREE, 1] = 0.25
     tract[DEGREE * 2, 2] = 0.25
 
-    samples_matrix = create_samples(DEGREE, NUM_LENGTH_SAMPLES, NUM_ACROSS_SAMPLES)
-    tangents_matrix = create_tangents(DEGREE, NUM_LENGTH_SAMPLES, NUM_ACROSS_SAMPLES)
+    samples_matrix = Tract.position_matrix(DEGREE, NUM_LENGTH_SAMPLES, NUM_RADIAL)
+    tangents_matrix = Tract.tangent_matrix(DEGREE, NUM_LENGTH_SAMPLES, NUM_RADIAL)
 
     positions = samples_matrix.dot(tract)
-    tangents = tangents_matrix.dot(tract)
-    lengths = np.sqrt(tangents[:, 0] ** 2 + tangents[:, 1] ** 2 + tangents[:, 2] ** 2)
-
-    indices = np.floor(positions)
-    remain = positions - indices
-    disps = ([], [], [])
-    for d in xrange(3):
-        for i in xrange((1 - INTERP_LENGTH), INTERP_LENGTH):
-            disps[d].append(remain[:, d] - i)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
