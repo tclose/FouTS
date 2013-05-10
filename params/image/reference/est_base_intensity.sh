d8214d18332d24572719c38793cbfb69adb2977e
Tom Close <tclose@oist.jp>
2013-05-10 09:30:33 +0900

    added reference tracts to params and fixed up select so that set properties are preserved

--- params/image/reference/est_base_intensity.sh
+++ params/image/reference/est_base_intensity.sh
@@ -1,5 +1,1 @@
-for f in */images/corpus_callosum.*0.mif; do d=donald/images; b=corpus_callosum.150.mif; n=corpus_callosum.150; n=150; for i in 1
-2
-3
-4
-5; do estimate_intensity donald/images/corpus_callosum.150.mif --reference_tract donald/images/../ref_tract.150.1.tct > donald/images/../intens.150.1.txt; echo done donald/images/corpus_callosum.150.mif; done; done
+for f in */images/corpus_callosum.*0.mif; do d=`dirname $f`; b=`basename $f`; n=${b%.mif}; n=${n#corpus_callosum.}; for i in `seq 5`; do estimate_intensity $f --reference_tract $d/../ref_tract.$n.$i.tct > $d/../intens.$n.$i.txt; done; done
