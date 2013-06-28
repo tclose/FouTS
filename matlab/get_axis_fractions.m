30a5f727c74a01d2f71544fa9d8233e9b5fd96df
Tom Close <tclose@oist.jp>
2013-06-28 10:08:31 +0900

    fixed up accurate strands_per_acs plotting

--- matlab/get_axis_fractions.m
+++ matlab/get_axis_fractions.m
@@ -1,56 +1,66 @@
 function [ax_fractions] = get_axis_fractions(num_strands)
-
-    dirname =fileparts(mfilename('fullpath'));
-    precalc_samples_location = [dirname '/precalc_axis_fractions.mat'];
-    if exist(precalc_samples_location, 'file')
-       precalc_samples = load(precalc_samples_location);
+    if num_strands == 0
+        ax_fractions = [];
     else
-       precalc_samples = cell(0);
-    end
-
-    if num_strands > length(precalc_samples)
-        for n=length(precalc_samples):num_strands
-            precalc_samples{n} = sample_evenly_on_disc(n);
+        dirname =fileparts(mfilename('fullpath'));
+        precalc_samples_location = [dirname '/precalc_axis_fractions.mat'];
+        if exist(precalc_samples_location, 'file')
+           load(precalc_samples_location, 'precalc_samples');
+        else
+           precalc_samples = cell(0);
         end
-        save(precalc_samples_location, precalc_samples)
-    end
-    ax_fractions = precalc_samples{num_strands};
 
+        if num_strands > length(precalc_samples)
+            for n=(length(precalc_samples)+1):num_strands
+                precalc_samples{n} = sample_evenly_on_disc(n);
+            end
+            save(precalc_samples_location, 'precalc_samples')
+        end
+        ax_fractions = precalc_samples{num_strands}';
+    end
 end
 
 function x = sample_evenly_on_disc(N)
-    % Create randomly distributed points within a circle
-    x0 = zeros(N,2);
-    step = 2 / floor(sqrt(N));
-    count = 0;
-    for x=-1:step1
-        for y=-1:step:1
-            if x^2 + y^2 < 1
-                count = count + 1;
-                x0(count,:) = [x, y];
+    if N == 1
+        x = [0, 0];
+    else
+        % Create randomly distributed points within a circle
+        x0 = zeros(N,2);
+        step = 2 / floor(sqrt(N));
+        count = 0;
+        for x=-1:step:1
+            for y=-1:step:1
+                if x^2 + y^2 < 1
+                    count = count + 1;
+                    x0(count,:) = [x, y];
+                end
             end
+        end 
+        circ_step = 2*pi / (N - count);
+        for theta = 0:circ_step:(2*pi - circ_step)
+            count = count + 1;
+            x0(count, :) = [cos(theta) sin(theta)];
         end
+        if count ~= N
+            error('count doesn''t equal N')
+        end
+        % Perform the optimisation
+        options = optimset('Algorithm', 'active-set', 'MaxFunEvals', 1e5,...
+                            'MaxIter', 1e3, 'TolCon', 1e-3);
+        x = fmincon(@fmin, x0, [], [], [], [], [], [], @fcon, options);
+        % Calculate the scale away from the edge of the tube
+        x = x * (1 - 1 / ceil(sqrt(N)));
     end
-    circ_step = 2*pi / (N - count);
-    for theta = 0:circ_step:(2*pi - circ_step)
-        count = count + 1;
-        x0(count, :) = [cos(theta) sin(theta)];
-    end
-    if count ~= N
-        error('count doesn''t equal N')
-    end
-    % Perform the optimisation
-    options = optimset('Algorithm', 'active-set', 'MaxFunEvals', 1e5,...
-                        'MaxIter', 1e3, 'TolCon', 1e-3);
-    x = fmincon(@fmin, x0, [], [], [], [], [], [], @fcon, options);
 end
 
 function y = fmin(x)
     X1 = repmat(x(:,1), [1, size(x,1)]);
     X2 = repmat(x(:,2), [1, size(x,1)]);
-    d = (X1 - X1').^2 + (X2 - X2').^2;
-    Y = triu(1./d.^4, 1);
+    d = ((X1 - X1').^2 + (X2 - X2').^2).^4;
+    Y = triu(1./d, 1);
     y = sum(Y(:));
+%      d = d + eye(length(x)) * 2;
+%      y = -max(min(d,[],2),[],1);
 end
 
 function [y, w] = fcon(x)
--- matlab/tracts2strands.m
+++ matlab/tracts2strands.m
@@ -32,7 +32,7 @@ function [strands, bundle_indices] = tracts2strands(tracts, base_widths, num_str
   % to be represented by whole number increments in number of strands 
   % rather than whole number increments in the square-root of strands
   if ~exist('acurate_acs','var')
-    accurate_acs = false; 
+    accurate_acs = true; 
   end
   
   if isempty(tract_indices)
@@ -56,7 +56,7 @@ function [strands, bundle_indices] = tracts2strands(tracts, base_widths, num_str
     end
   
     if accurate_acs
-       ax_fractions = get_axis_fractions(strands_per_acs * acs);
+       ax_fractions = get_axis_fractions(round(strands_per_acs * acs(tract_i)));
     else
         if strands_per_acs ~= -1
           num_strands = round(sqrt(strands_per_acs * acs));    
@@ -68,36 +68,38 @@ function [strands, bundle_indices] = tracts2strands(tracts, base_widths, num_str
         ax_fractions = [ax_frac_col; ax_frac_col];
     end
     
-    for ax2_frac = ax_fractions(1, :)
+    if ~isempty(ax_fractions)
+      for ax2_frac = ax_fractions(1, :)
 
-      for ax3_frac = ax_fractions(2, :)
+        for ax3_frac = ax_fractions(2, :)
 
-        if (sqrt(ax2_frac^2 + ax3_frac^2) <= 1.0) || oblong
-          strand_count = strand_count + 1;
+          if (sqrt(ax2_frac^2 + ax3_frac^2) <= 1.0) || oblong
+            strand_count = strand_count + 1;
 
-          strands{strand_count} = tracts{tract_i, 1} + (tracts{tract_i, 2} * ax2_frac + tracts{tract_i, 3} * ax3_frac) * base_widths(tract_i);
-          
-          if highlight_axes
-            
-            if ax2_frac == 0 && ax3_frac == 0
-              strand_index = base_index + 1;
-            elseif ax2_frac == 0
-              strand_index = base_index + 2;
-            elseif ax3_frac == 0
-              strand_index = base_index + 3;
+            strands{strand_count} = tracts{tract_i, 1} + (tracts{tract_i, 2} * ax2_frac + tracts{tract_i, 3} * ax3_frac) * base_widths(tract_i);
+
+            if highlight_axes
+
+              if ax2_frac == 0 && ax3_frac == 0
+                strand_index = base_index + 1;
+              elseif ax2_frac == 0
+                strand_index = base_index + 2;
+              elseif ax3_frac == 0
+                strand_index = base_index + 3;
+              else
+                strand_index = base_index;
+              end
+
             else
               strand_index = base_index;
             end
-              
-          else
-            strand_index = base_index;
+
+            bundle_indices(strand_count) = strand_index;
+
           end
-          
-          bundle_indices(strand_count) = strand_index;
-          
         end
-      end
-    end 
+      end 
+    end
 
   end  
 
