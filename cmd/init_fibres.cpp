9ccfe98ddbe829c28fa14d8c4cfdcb9f414811d3
Tom Close <tclose@oist.jp>
2014-01-20 11:17:45 +0900

    fixed up offset of init fibres to be 0,0,0 instead of the corner offset (the roi is already offset)

--- cmd/init_fibres.cpp
+++ cmd/init_fibres.cpp
@@ -232,7 +232,7 @@ EXECUTE {
         SET_IMAGE_PARAMETERS;
         
         if (!img_offsets.valid())
-            img_offsets = Image::Observed::Buffer::default_corner_offset(img_dims, img_vox_lengths);
+            img_offsets = Triple<double>(0.0, 0.0, 0.0); // Image::Observed::Buffer::default_corner_offset(img_dims, img_vox_lengths);
         
         Triple<double> roi_extent = (img_vox_lengths * img_dims) / 2.0 - edge_buffer;
         
