1ea682a1d976bc93f4d70baf9b16668208d621c5
Tom Close <tclose@oist.jp>
2015-02-08 19:08:02 +0900

    added FouTS directory to lib path

--- python/fouts/hpc/__init__.py
+++ python/fouts/hpc/__init__.py
@@ -128,11 +128,14 @@ def create_env(work_dir):
     @param work_dir: The work directory to set the envinroment variables for
     """
     env = os.environ.copy()
-    env['PATH'] = env['PATH'] + os.pathsep + \
-                  os.path.join(env['HOME'], 'git', 'FouTS', 'bin') + os.pathsep + \
-                  os.path.join(env['HOME'], 'git', 'MRtrix', 'bin')
-    env['LD_LIBRARY_PATH'] = '/opt/mpi/gnu/openmpi-1.6.3/lib' + os.pathsep + \
-                  os.path.join(env['HOME'], 'git', 'MRtrix', 'lib')
+    env['PATH'] = (
+        env['PATH'] + os.pathsep +
+        os.path.join(env['HOME'], 'git', 'FouTS', 'bin') + os.pathsep +
+        os.path.join(env['HOME'], 'git', 'MRtrix', 'bin'))
+    env['LD_LIBRARY_PATH'] = (
+        '/opt/mpi/gnu/openmpi-1.6.3/lib' + os.pathsep +
+        os.path.join(env['HOME'], 'git', 'FouTS', 'lib') + os.pathsep +
+        os.path.join(env['HOME'], 'git', 'MRtrix', 'lib'))
     return env
 
 
